52643db32a4f13d2938749c4f506b509d1ecb5b6
Massimiliano Giovagnoli <me@maxgio.it>
2023-11-23 18:49:41 +0100

    refactor(cmd): move error handling to main
    Signed-off-by: Massimiliano Giovagnoli <me@maxgio.it>

--- main.go
+++ main.go
@@ -1,9 +1,17 @@
 package main
 
 import (
+	"fmt"
 	"github.com/maxgio92/homework-object-storage/cmd/serve"
+	"os"
 )
 
 func main() {
-	serve.Execute()
+	cmd := serve.NewCmd()
+
+	err := cmd.Execute()
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
 }
